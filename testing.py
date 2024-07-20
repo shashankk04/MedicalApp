5af123a1f847d92d66de1c468633de25bba07e2e
invcble <imonbera13@gmail.com>
2024-07-20 14:25:28 -0400

    update

--- testing.py
+++ testing.py
@@ -5,6 +5,8 @@ load_dotenv()
 
 from langchain_google_community import GoogleSearchAPIWrapper
 from langchain_core.tools import Tool
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_groq import ChatGroq
 
 search = GoogleSearchAPIWrapper(k=5)
 
@@ -14,4 +16,18 @@ tool = Tool(
     func=search.run,
 )
 
-print(tool.run("Hospital near 42nd street Philadelphia"))
+CONTEXT = tool.run("Hospital near 42nd street Philadelphia")
+
+
+LLM = ChatGroq(
+    temperature=0,
+    model="llama3-70b-8192",
+    # api_key="" # Optional if not set as an environment variable
+)
+
+prompt = ChatPromptTemplate.from_template("You are a smart assistant")
+
+# chain = prompt | LLM
+# data = chain.invoke({"text": "what is the capital of India?"})
+data =LLM.invoke("what is the capital of India?")
+print(data.content)
