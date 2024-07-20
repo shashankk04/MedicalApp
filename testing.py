14cb8ea44e4ff85cd5a0bf124051f2172f0370a1
invcble <imonbera13@gmail.com>
2024-07-20 15:27:48 -0400

    update

--- testing.py
+++ testing.py
@@ -5,29 +5,58 @@ load_dotenv()
 
 from langchain_google_community import GoogleSearchAPIWrapper
 from langchain_core.tools import Tool
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_groq import ChatGroq
 
-search = GoogleSearchAPIWrapper(k=5)
+# search = GoogleSearchAPIWrapper(k=5)
 
-tool = Tool(
-    name="google_search",
-    description="Search Google for recent results.",
-    func=search.run,
-)
+# tool = Tool(
+#     name="google_search",
+#     description="Search Google for recent results.",
+#     func=search.run,
+# )
 
-CONTEXT = tool.run("Hospital near 42nd street Philadelphia")
+# CONTEXT = tool.run("Hospital near 42nd street Philadelphia")
 
 
-LLM = ChatGroq(
-    temperature=0,
+from groq import Groq
+
+client = Groq()
+
+chat_completion = client.chat.completions.create(
+   
+    messages=[
+        {
+            "role": "system",
+            "content": "You are a helpful voice assistant with web search capability. You always return a JSON object with two keys ONLY. The first key contains either 'yes' or 'no' indicating whether you need a web search to answer the question. If the first key's value is 'yes', the second key contains an effective web search query. If the first key's value is 'no', the second key contains a concise answer to the question."
+
+        },
+        {
+            "role": "user",
+            "content": "rain chances in USA today",
+        }
+    ],
+
     model="llama3-70b-8192",
-    # api_key="" # Optional if not set as an environment variable
+    temperature=0,
+    max_tokens=1024,
+    seed=42,
+    top_p=0.1,
+
+    # response_format={
+    #     "type": "json_object",
+    #     "schema": {
+    #         "type": "object",
+    #         "properties": {"need_search": {"type": "string"}, "answer": {"type": "string"},},
+    #         "required": ["need_search", "answer"],
+    #     },
+    # }
+
+    # A stop sequence is a predefined or user-specified text string that
+    # signals an AI to stop generating content, ensuring its responses
+    # remain focused and concise. Examples include punctuation marks and
+    # markers like "[end]".
+    # stop=None,
 )
 
-prompt = ChatPromptTemplate.from_template("You are a smart assistant")
-
-# chain = prompt | LLM
-# data = chain.invoke({"text": "what is the capital of India?"})
-data =LLM.invoke("what is the capital of India?")
-print(data.content)
+# Print the completion returned by the LLM.
+print(type(chat_completion.choices[0].message.content))
+# print[newlist[newlist.find("need_search"):newlist.find("need_search")+4]]
