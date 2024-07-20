a34404ba1c0fa6137b2d60933555fded1d6787d2
invcble <imonbera13@gmail.com>
2024-07-20 10:22:07 -0400

    update

--- testing.py
+++ testing.py
@@ -1,6 +1,17 @@
-import json
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-with open('API_key.json', 'r') as f:
-    keys = json.load(f)
 
-print(keys)
+from langchain_google_community import GoogleSearchAPIWrapper
+from langchain_core.tools import Tool
+
+search = GoogleSearchAPIWrapper(k=5)
+
+tool = Tool(
+    name="google_search",
+    description="Search Google for recent results.",
+    func=search.run,
+)
+
+print(tool.run("Hospital near 42nd street Philadelphia"))
