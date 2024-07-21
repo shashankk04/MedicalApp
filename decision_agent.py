7e32d8bf097b80afb47e35c1b946672a712dda58
invcble <imonbera13@gmail.com>
2024-07-20 23:06:14 -0400

    update

---
+++ decision_agent.py
@@ -0,0 +1,31 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain_google_community import GoogleSearchAPIWrapper
+from langchain_core.tools import Tool
+
+import llama_call
+
+Q = "Hi! my name is Imon and I live in Philly. I am having sudden pain in my wrist. Where should I go?"
+
+def decide(question):
+    llm_response = llama_call.process(question)
+    print(llm_response)
+
+    if llm_response['need_search'] == 'no':
+                return llm_response['answer']
+    else:
+        web_query = llm_response['answer']
+
+        search = GoogleSearchAPIWrapper(k=10)
+        tool = Tool(
+            name="google_search",
+            description="Search Google for recent results.",
+            func=search.run,
+        )
+
+        return tool.run(web_query)
+
+
+print(decide(Q))
---
+++ llama_call.py
@@ -0,0 +1,45 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from groq import Groq
+import json
+
+client = Groq()
+
+def process(Query):
+    chat_completion = client.chat.completions.create(
+    
+        messages=[
+            {
+                "role": "system",
+                "content": "You are a helpful voice assistant with web search capability. You always return a JSON object with two keys ONLY ('need_search' and 'answer':). The first key contains either 'yes' or 'no' indicating whether you need a web search to answer the question. If the first key's value is 'yes', the second key contains an effective web search query. If the first key's value is 'no', the second key contains a concise answer to the question."
+            },
+            {
+                "role": "user",
+                "content": Query,
+            }
+        ],
+
+        model="llama3-70b-8192",
+        temperature=0,
+        max_tokens=1024,
+        seed=42,
+        top_p=0.1,
+
+        response_format={
+            "type": "json_object",
+            "schema": {
+                "type": "object",
+                "properties": {"need_search": {"type": "string"}, "answer": {"type": "string"}},
+                "required": ["need_search", "answer"],
+            },
+        }
+
+        # A stop sequence is a predefined or user-specified text string that
+        # signals an AI to stop generating content, ensuring its responses
+        # remain focused and concise. Examples include punctuation marks and
+        # markers like "[end]".
+        # stop=None,
+    )
+    data = chat_completion.choices[0].message.content
+    return json.loads(data)
